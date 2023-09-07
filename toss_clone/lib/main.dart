527e2dd26137746d7b48da427ef5238d9db61f9f
hangunhee39 <ghee0831@naver.com>
2023-09-07 13:54:17 +0900

    1. slpash

--- toss_clone/lib/main.dart
+++ toss_clone/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 import 'common/data/preference/app_preferences.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  //Flutter NativeSplash 화면 유지 (로그인 등 사용)
+  final bindings = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: bindings);
+
   await EasyLocalization.ensureInitialized();
   await AppPreferences.init();
 
